32f3ffb6960b8d0bdeef47a7dc0ce543c89a0276
Kaliane92 <kevin_aliane@hotmail.fr>
2022-05-15 18:43:10 +0200

    test

--- old/InSpace/js/plyr.js
+++
@@ -1,8 +0,0 @@
-import Plyr from 'plyr';
-
-const player = new Plyr('#player');
-// Change the second argument to your options:
-// https://github.com/sampotts/plyr/#options
-
-// Expose player so it can be used from the console
-window.player = player;
