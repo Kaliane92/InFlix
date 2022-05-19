76de432ac7d8200a9991e23624b9f71b83e01953
Kaliane92 <kevin_aliane@hotmail.fr>
2022-05-19 09:29:40 +0200

    remove-assets

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Slider from '../slider/index'
+
 
 const Home = () => {
     return (
         <section className='content'>
-            <Slider
-                publisher={'Marvel'}
-            />
+
         </section>
     )
 }
